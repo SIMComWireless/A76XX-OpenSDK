c4c6d57d3b0f9063ab048ff2383aa2b98df604a0
jack <yanan.sun@simcom.com>
2026-04-21 12:10:13 +0800

    Add MQTTS Demo

--- examples/mqtts/demo_helloworld.c
+++ examples/mqtts/demo_helloworld.c
@@ -1,11 +1,3 @@
-/*
- * @Author: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @Date: 2024-12-31 15:59:15
- * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
- * @LastEditTime: 2025-03-28 10:50:50
- * @FilePath: \A110B01V11A7672M7_SDK_CUS_RU_241231\SIMCOM_SDK_SET\sc_demo\V1\src\demo_helloworld.c
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 /**
   ******************************************************************************
   * @file    demo_helloworld.c
